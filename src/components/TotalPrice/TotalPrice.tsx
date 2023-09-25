2bd31f97ac5aa057e06614efe0733196ef2eb8eb
refiloematlapeng <matlapengrp@gmail.com>
2023-09-25 15:09:06 +0200

    ✨ Modify price title

--- src/components/TotalPrice/TotalPrice.tsx
+++ src/components/TotalPrice/TotalPrice.tsx
@@ -8,5 +8,5 @@ interface Props {
 }
 
 export const TotalPrice: FunctionComponent<Props> = ({ amount }) => {
-return <div className={classes.totalPrice}>Total: {<CurrencyFormatter amount={amount} />}</div>
+return <div className={classes.totalPrice}>Total Price: {<CurrencyFormatter amount={amount} />}</div>
 }
